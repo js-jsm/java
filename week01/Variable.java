1112942c1d41f9a1ae14fa911efc4b1d73da1230
dolen(choi tae san) <imcts@coupang.com>
2017-03-25 20:22:07 +0900

    first commit

---
+++ week01/Variable.java
@@ -0,0 +1,10 @@
+package jsmine.task.week01;
+
+/**
+ * Created by imcts on 2017. 3. 25..
+ */
+public class Variable {
+    public static void main(String[] args) {
+        System.out.println("start");
+    }
+}
---
+++ week02/ConditionAndIterator.java
@@ -0,0 +1,7 @@
+package jsmine.task.week02;
+
+/**
+ * Created by imcts on 2017. 3. 25..
+ */
+public class ConditionAndIterator {
+}
