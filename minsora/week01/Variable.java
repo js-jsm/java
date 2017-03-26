c771f7f7a3d5f6330a443cfe146a8b66d3250e6c
minsora <ssola129@gmail.com>
2017-03-26 13:59:01 +0900

    java study week01 homework Variable.java

---
+++ minsora/week01/Variable.java
@@ -0,0 +1,10 @@
+package minsora.variable;
+
+public class Variable {
+	public static void main(String [] args) {
+		int intVariable = 890129;
+        Integer intClass = 890129;
+        
+        System.out.println(intVariable);
+	}
+}
